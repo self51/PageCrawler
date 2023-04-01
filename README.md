44e00fb7f3977960f2e16db6b51dcd9439f0b47b
se1f4 <58149320+se1f4@users.noreply.github.com>
2023-04-01 15:57:07 +0300

    Created script to collect data from web pages, and send it to a Telegram channel

---
+++ README.md
@@ -0,0 +1,24 @@
+# **Page crawler**
+
+<br/>
+
+#### About
+This is a pet-project, it should not be used for commercial purposes!
+<br/>Page Crawler is a script to automatically collect data from web pages, 
+<br/>and send it to a Telegram channel.
+
+##### Technology stack:
+* Python 3.8;
+* Redis;
+
+##### Getting Started
+* Clone the repository;
+* Go to the project directory;
+* Make sure you have Redis installed, set up Redis;
+* Set up Telegram API;
+  * You need to have: TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID;
+* Create your .env file;
+  * File must contain: TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, REDIS_HOST, REDIS_PORT;
+* `$ pip install -r requirements.txt`.
+
+Made by `Self`.
---
+++ headline_hunter.py
@@ -0,0 +1,55 @@
+import time
+import asyncio
+import redis
+import environ
+
+import requests
+from telegram import Bot
+from bs4 import BeautifulSoup
+
+# Set up environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+# Set up Redis
+redis_client = redis.Redis(host=env('REDIS_HOST'), port=env('REDIS_PORT'), db=0)
+redis_key = 'previous_titles'
+
+# Set up Telegram bot
+bot = Bot(token=env('TELEGRAM_BOT_TOKEN'))
+chat_id = env('TELEGRAM_CHAT_ID')
+
+
+# Scrapes recent articles and add them to redis
+def get_latest_article():
+    url = 'https://www.tesmanian.com/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    latest_article = set()
+    for article in soup.select('blog-post-card'):
+        title = article.find('p').find('a').text
+        link = article.find('a')['href']
+        if not redis_client.sismember(redis_key, link):
+            latest_article.add((title, f'https://www.tesmanian.com{link}'))
+            redis_client.sadd(redis_key, link)
+
+    return latest_article
+
+
+async def send_message():
+    messages = get_latest_article()
+    for message in messages:
+        await bot.send_message(chat_id=chat_id, text=f'Title - {list(message)[0]}, Link - {list(message)[1]}')
+        time.sleep(1)
+
+
+def main():
+    while True:
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+        asyncio.run(send_message())
+        time.sleep(15)
+
+
+if __name__ == '__main__':
+    main()
